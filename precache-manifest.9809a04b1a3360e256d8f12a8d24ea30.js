bc1c2e104079243191f921ac9bae04c3cd810ef3
Devin <guozwhero@126.com>
2023-09-14 00:13:19 +0800

    66
    66

---
+++ precache-manifest.9809a04b1a3360e256d8f12a8d24ea30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f42be8a547bf4a8206a35e00db317e4",
+    "url": "/near_crowdfunding_pages/index.html"
+  },
+  {
+    "revision": "ab01cd7ab296c2396fac",
+    "url": "/near_crowdfunding_pages/static/css/main.711ea19d.chunk.css"
+  },
+  {
+    "revision": "c6f62fab971c6964bfe7",
+    "url": "/near_crowdfunding_pages/static/js/2.4d3b9ce2.chunk.js"
+  },
+  {
+    "revision": "3bb3494124db52eabe35442096be882b",
+    "url": "/near_crowdfunding_pages/static/js/2.4d3b9ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab01cd7ab296c2396fac",
+    "url": "/near_crowdfunding_pages/static/js/main.18f8fc6c.chunk.js"
+  },
+  {
+    "revision": "0fcf3e388e20a667685c",
+    "url": "/near_crowdfunding_pages/static/js/runtime-main.62b9b0ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/near_crowdfunding_demo/precache-manifest.1dce339e1a98946317cf578047735034.js"
+  "/near_crowdfunding_pages/precache-manifest.9809a04b1a3360e256d8f12a8d24ea30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/near_crowdfunding_demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/near_crowdfunding_pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
