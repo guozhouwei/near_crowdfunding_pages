5b096cbe48c10e890d3c4230b2cdf00873b4eb2e
Devin <guozwhero@126.com>
2023-09-17 01:24:47 +0800

    66
    66

---
+++ precache-manifest.3920d372391735a8c1acf6fde7f4c546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09438435279eeef5ddde5197c3a3be20",
+    "url": "/near_crowdfunding_pages/index.html"
+  },
+  {
+    "revision": "b4de8b288d745244c16b",
+    "url": "/near_crowdfunding_pages/static/css/main.711ea19d.chunk.css"
+  },
+  {
+    "revision": "f81799344c7a74f6367d",
+    "url": "/near_crowdfunding_pages/static/js/2.65a56496.chunk.js"
+  },
+  {
+    "revision": "3bb3494124db52eabe35442096be882b",
+    "url": "/near_crowdfunding_pages/static/js/2.65a56496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4de8b288d745244c16b",
+    "url": "/near_crowdfunding_pages/static/js/main.4fdbdba5.chunk.js"
+  },
+  {
+    "revision": "0fcf3e388e20a667685c",
+    "url": "/near_crowdfunding_pages/static/js/runtime-main.62b9b0ef.js"
+  }
+]);
